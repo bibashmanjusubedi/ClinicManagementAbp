4a7219dcfcfb099bd37423dd26bea2c7170f8998
bibashmanjusubedi <acharabibash@outlook.com>
2025-12-18 00:44:24 +0545

    feat: PatientApp Service interface and implementation

---
+++ src/ClinicManagementAbp.Application.Contracts/Patients/IPatientAppService.cs
@@ -0,0 +1,19 @@
+﻿using ClinicManagementAbp.Patients.Dtos;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace ClinicManagementAbp.Patients
+{
+    public interface IPatientAppService : ICrudAppService<
+        PatientDto,
+        Guid,
+        PagedAndSortedResultRequestDto,
+        CreateUpdatePatientDto>
+    {
+        Task SoftDeleteAsync(Guid id);
+        Task<PagedResultDto<PatientDto>> WithDetailsAsync(PagedAndSortedResultRequestDto input);  // Your WithDetailsAsync()
+        Task<PatientDto> GetActiveByIdAsync(Guid id);
+    }
+}
---
+++ src/ClinicManagementAbp.Application/Patients/PatientAppService.cs
@@ -0,0 +1,77 @@
+﻿using ClinicManagementAbp.Patients.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.ObjectMapping;
+
+namespace ClinicManagementAbp.Patients
+{
+    public class PatientAppService:
+        CrudAppService<Patient,PatientDto,Guid,
+            PagedAndSortedResultRequestDto,CreateUpdatePatientDto>, IPatientAppService
+    {
+        private readonly IPatientRepository _patientRepository;
+
+
+        public PatientAppService(
+            IRepository<Patient,Guid>.Default repository,
+            IPatientRepository patientRepository)
+            : base(repository)
+        {
+            _patientRepository = patientRepository;
+        }
+
+        // override standard delete
+        public override async Task DeleteAsync(Guid id)
+        {
+            await _patientRepository.SoftDeleteAsync(id);
+        }
+
+        public async Task SoftDeleteAsync(Guid id)
+        {
+            await _patientRepository.SoftDeleteAsync(id);
+        }
+
+
+        public async Task<PagedResultDto<PatientDto>> WithDetailsAsync(PagedAndSortedResultRequestDto input)
+        {
+            var queryable = await _patientRepository.WithDetailsAsync();
+
+            var totalCount = await AsyncExecuter.CountAsync(queryable);
+
+            queryable = queryable
+                .OrderBy(input.Sorting ?? "CreationTime desc")
+                .PageBy(input);
+
+
+            var entities = await AsyncExecuter.ToListAsync(queryable);
+            var dtos = ObjectMapper.Map<List<Patient>, List<PatientDto>>(entities);
+
+
+            return new PagedResultDto<PatientDto>
+            {
+                TotalCount = totalCount,
+                Items = dtos
+            };
+
+        }
+
+        public async Task<PatientDto> GetActiveByIdAsync(Guid id)
+        {
+            var patient = await _patientRepository.GetActiveByIdAsync(id);
+
+            if (patient == null)
+            {
+                throw new EntityNotFoundException(typeof(Patient), id);
+            }
+
+            return ObjectMapper.Map<Patient, PatientDto>(patient);
+        }
+
+    }
+}
