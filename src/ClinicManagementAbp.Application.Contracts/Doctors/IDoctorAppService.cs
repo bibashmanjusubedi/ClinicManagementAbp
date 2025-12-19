42886d84bfae542d7dd05776ed472af11f8bc093
bibashmanjusubedi <acharabibash@outlook.com>
2025-12-19 06:49:27 +0545

    feat(doctors): add DoctorAppService and IDoctorAppService for CRUD operations

---
+++ src/ClinicManagementAbp.Application.Contracts/Doctors/IDoctorAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using ClinicManagementAbp.Doctors.Dtos;
+using Volo.Abp.Application.Dtos;
+
+namespace ClinicManagementAbp.Doctors;
+
+public interface IDoctorAppService: ICrudAppService<
+    DoctorDto,
+    Guid,
+    PagedAndSortedResultRequestDto,
+    CreateUpdateDoctorDto,
+    CreateUpdateDoctorDto>
+{
+
+}
---
+++ src/ClinicManagementAbp.Application/Doctors/DoctorAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+using ClinicManagementAbp.Doctors.Dtos;
+using ClinicManagementAbp.Doctors;
+using Volo.Abp.Application.Dtos;
+
+namespace ClinicManagementAbp.Doctors;
+
+
+public class DoctorAppService : CrudAppService<Doctor,DoctorDto,Guid,PagedAndSortedResultRequestDto,CreateUpdateDoctorDto,CreateUpdateDoctorDto>,
+    IDoctorAppService
+{
+    public DoctorAppService(IRepository<Doctor, Guid> repository)
+        : base(repository)
+    {
+    }
+
+}
