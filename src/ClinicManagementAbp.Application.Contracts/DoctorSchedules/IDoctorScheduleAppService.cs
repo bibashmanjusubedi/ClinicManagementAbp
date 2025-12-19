1a03250fbed14bf7e77568337861d384f455de43
bibashmanjusubedi <acharabibash@outlook.com>
2025-12-19 07:13:44 +0545

    feat(doctor):IDoctorAppService

---
+++ src/ClinicManagementAbp.Application.Contracts/DoctorSchedules/IDoctorScheduleAppService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using ClinicManagementAbp.DoctorSchedules.Dtos;
+using Volo.Abp.Application.Dtos;
+
+namespace ClinicManagementAbp.DoctorSchedules;
+
+public interface IDoctorScheduleAppService: ICrudAppService<
+    DoctorScheduleDto,
+    Guid,
+    PagedAndSortedResultRequestDto,
+    CreateUpdateDoctorScheduleDto,
+    CreateUpdateDoctorScheduleDto>
+{
+    Task<PagedResultDto<DoctorScheduleDto>> GetDoctorSchedulesByDoctorAsync(Guid doctorId, PagedAndSortedResultRequestDto input);
+    Task<PagedResultDto<DoctorScheduleByDoctorDto>> GetDoctorSchedulesByDoctorIdAsync(Guid doctorId, PagedAndSortedResultRequestDto input);
+}
